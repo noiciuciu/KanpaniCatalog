bf2d5bf81a0c51d62e74a0ee842786052bcaa697
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-04-29 16:32:47 +0800

    fix

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller {
     
